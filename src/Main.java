1c6733fd3aab320d5d4866476cf1de4fe6efb6d7
Akshi Vasaniya <vasaniyaakshi8@gmail.com>
2026-04-26 09:33:45 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Student Management System");
+        System.out.println();
+
+//        startFlow();
+    }
+}
