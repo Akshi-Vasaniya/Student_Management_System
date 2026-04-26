88588baeb740c3cd170fe2a8fb26d2b860814822
Akshi Vasaniya <vasaniyaakshi8@gmail.com>
2026-04-26 12:41:22 +0530

    Store students records in CSV file

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
         String[] header = {"Id", "Name", "Age", "Gender", "Course"};
         File file = new File("src/Data/StudentRecords.csv");
         if (file.length() == 0) {
-//            addDataToCSV(header, path);
+            addDataToCSV(header, path);
         }
 
         // Operations on file
@@ -33,8 +33,8 @@ public class Main {
 
         switch (sc.nextInt()) {
             case 1:
-//                addStudent(sc, path);
-//                break;
+                addStudent(sc, path);
+                break;
             case 2:
 //                List<String[]> rows = fetchStudent(path);
 //                for (String[] row: rows) {
@@ -53,6 +53,48 @@ public class Main {
             case 5:
 //                deleteRecord(path);
                 break;
+        }
+    }
+
+    /**
+     * This method is used to fill up the student details. To create the array of records.
+     * @param sc Scanner object
+     * @param path Path of the CSV file.
+     */
+    private static void addStudent(Scanner sc, String path) {
+        sc.nextLine();
+        System.out.println("Enter the details of the student - ");
+        String[] arr = new String[5];
+        System.out.print("ID: ");
+        arr[0] = sc.nextLine();
+        System.out.print("Name: ");
+        arr[1] = sc.nextLine();
+        System.out.print("Age: ");
+        arr[2] = sc.nextLine();
+        System.out.print("Gender: ");
+        arr[3] = sc.nextLine();
+        System.out.print("Course: ");
+        arr[4] = sc.nextLine();
+
+        addDataToCSV(arr, path);
+    }
+
+    /**
+     * This method is used to store records to CSV file. Using CSVWriter library to store the input in CSV file.
+     * @param record Student record array
+     * @param path Path of CSV file
+     */
+    private static void addDataToCSV(String[] record, String path) {
+        try{
+            FileWriter fw = new FileWriter(path, true); // Create the file in not exists
+            CSVWriter csvWriter = new CSVWriter(fw);
+            csvWriter.writeNext(record);
+            csvWriter.close();
+            System.out.println();
+            System.out.println("Record added successfully!");
+            System.out.println();
+        } catch (Exception ex) {
+            System.out.println("addDataToCSV() | Exception: "+ ex.getMessage());
         }
     }
 }
