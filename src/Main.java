86ea415c5c2f703ff0ce5d64d7ded827338ca1e7
Akshi Vasaniya <vasaniyaakshi8@gmail.com>
2026-04-26 09:52:10 +0530

    Start workflow program

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,46 @@ public class Main {
         System.out.println("Student Management System");
         System.out.println();
 
-//        startFlow();
+        startFlow();
+    }
+
+    private static void startFlow() {
+        String path = "src/Data/StudentRecords.csv";
+        // Add header to file
+        String[] header = {"Id", "Name", "Age", "Gender", "Course"};
+        File file = new File("src/Data/StudentRecords.csv");
+        if (file.length() == 0) {
+//            addDataToCSV(header, path);
+        }
+
+        // Operations on file
+        System.out.println("Select the Option: ");
+        System.out.println("1. Add Student\n2. Show Records\n3. Search in records\n4. Update record\n5. Delete record");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("=> ");
+
+        switch (sc.nextInt()) {
+            case 1:
+//                addStudent(sc, path);
+//                break;
+            case 2:
+//                List<String[]> rows = fetchStudent(path);
+//                for (String[] row: rows) {
+//                    for (String col: row) {
+//                        System.out.print(col + "\t");
+//                    }
+//                    System.out.println();
+//                }
+                break;
+            case 3:
+//                searchRecords(sc, path);
+                break;
+            case 4:
+//                updateRecord(path);
+                break;
+            case 5:
+//                deleteRecord(path);
+                break;
+        }
     }
 }
